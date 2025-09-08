20a2aa60e6dc11b9678ef424f8b31c329c0415d1
Universe IT-ST <universeit343@gmail.com>
2025-09-08 14:30:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
